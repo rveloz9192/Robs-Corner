e94b8842a7b03fe7eadc407cc39dca2bb2103e68
rveloz9192 <51169144+rveloz9192@users.noreply.github.com>
2019-12-10 18:26:08 -0700

    Add files via upload

---
+++ RSSI/Main.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace RSSI
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.pnlHeader = new System.Windows.Forms.Panel();
+            this.btnMinimize = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lblHeader = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.LOGO = new System.Windows.Forms.PictureBox();
+            this.txtSerialNumber = new System.Windows.Forms.TextBox();
+            this.lblSerialNumber = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.chcklstEnabled = new System.Windows.Forms.CheckedListBox();
+            this.btnEnable = new System.Windows.Forms.Button();
+            this.lstbxRSSI = new System.Windows.Forms.ListBox();
+            this.pnlHeader.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.LOGO)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlHeader
+            // 
+            this.pnlHeader.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.pnlHeader.Controls.Add(this.btnMinimize);
+            this.pnlHeader.Controls.Add(this.btnClose);
+            this.pnlHeader.Controls.Add(this.lblHeader);
+            this.pnlHeader.Controls.Add(this.panel2);
+            this.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlHeader.Location = new System.Drawing.Point(0, 0);
+            this.pnlHeader.Margin = new System.Windows.Forms.Padding(5);
+            this.pnlHeader.Name = "pnlHeader";
+            this.pnlHeader.Size = new System.Drawing.Size(1315, 50);
+            this.pnlHeader.TabIndex = 9;
+            this.pnlHeader.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PnlHeader_MouseDown);
+            this.pnlHeader.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PnlHeader_MouseMove);
+            this.pnlHeader.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PnlHeader_MouseUp);
+            // 
+            // btnMinimize
+            // 
+            this.btnMinimize.FlatAppearance.BorderSize = 0;
+            this.btnMinimize.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(204)))), ((int)(((byte)(204)))));
+            this.btnMinimize.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(229)))), ((int)(((byte)(229)))));
+            this.btnMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMinimize.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMinimize.ForeColor = System.Drawing.Color.Black;
+            this.btnMinimize.Location = new System.Drawing.Point(1243, 0);
+            this.btnMinimize.Name = "btnMinimize";
+            this.btnMinimize.Size = new System.Drawing.Size(32, 28);
+            this.btnMinimize.TabIndex = 6;
+            this.btnMinimize.TabStop = false;
+            this.btnMinimize.Text = "__";
+            this.btnMinimize.UseVisualStyleBackColor = true;
+            this.btnMinimize.Click += new System.EventHandler(this.BtnMinimize_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.FlatAppearance.BorderSize = 0;
+            this.btnClose.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(101)))), ((int)(((byte)(113)))));
+            this.btnClose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(17)))), ((int)(((byte)(35)))));
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.ForeColor = System.Drawing.Color.Black;
+            this.btnClose.Location = new System.Drawing.Point(1280, 0);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(32, 28);
+            this.btnClose.TabIndex = 5;
+            this.btnClose.TabStop = false;
+            this.btnClose.Text = "x";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.BtnClose_Click);
+            // 
+            // lblHeader
+            // 
+            this.lblHeader.AutoSize = true;
+            this.lblHeader.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeader.ForeColor = System.Drawing.Color.DimGray;
+            this.lblHeader.Location = new System.Drawing.Point(114, 20);
+            this.lblHeader.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblHeader.Name = "lblHeader";
+            this.lblHeader.Size = new System.Drawing.Size(165, 30);
+            this.lblHeader.TabIndex = 7;
+            this.lblHeader.Text = "RSSI Analyzer";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.LOGO);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(5);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(115, 50);
+            this.panel2.TabIndex = 0;
+            // 
+            // LOGO
+            // 
+            this.LOGO.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(68)))), ((int)(((byte)(139)))));
+            this.LOGO.Image = ((System.Drawing.Image)(resources.GetObject("LOGO.Image")));
+            this.LOGO.Location = new System.Drawing.Point(0, 0);
+            this.LOGO.Margin = new System.Windows.Forms.Padding(6);
+            this.LOGO.Name = "LOGO";
+            this.LOGO.Size = new System.Drawing.Size(115, 50);
+            this.LOGO.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.LOGO.TabIndex = 3;
+            this.LOGO.TabStop = false;
+            // 
+            // txtSerialNumber
+            // 
+            this.txtSerialNumber.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtSerialNumber.Location = new System.Drawing.Point(14, 97);
+            this.txtSerialNumber.Margin = new System.Windows.Forms.Padding(5);
+            this.txtSerialNumber.Name = "txtSerialNumber";
+            this.txtSerialNumber.Size = new System.Drawing.Size(101, 20);
+            this.txtSerialNumber.TabIndex = 0;
+            this.txtSerialNumber.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TxtSerialNumber_KeyDown);
+            // 
+            // lblSerialNumber
+            // 
+            this.lblSerialNumber.AutoSize = true;
+            this.lblSerialNumber.Location = new System.Drawing.Point(12, 71);
+            this.lblSerialNumber.Name = "lblSerialNumber";
+            this.lblSerialNumber.Size = new System.Drawing.Size(121, 21);
+            this.lblSerialNumber.TabIndex = 2;
+            this.lblSerialNumber.Text = "Serial Number:";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(68)))), ((int)(((byte)(139)))));
+            this.btnAdd.FlatAppearance.BorderSize = 0;
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.ForeColor = System.Drawing.Color.White;
+            this.btnAdd.Location = new System.Drawing.Point(123, 91);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(83, 33);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(220, 97);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(1069, 547);
+            this.chart1.TabIndex = 11;
+            this.chart1.Text = "chart1";
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(68)))), ((int)(((byte)(139)))));
+            this.btnRemove.FlatAppearance.BorderSize = 0;
+            this.btnRemove.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRemove.ForeColor = System.Drawing.Color.White;
+            this.btnRemove.Location = new System.Drawing.Point(16, 399);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(83, 33);
+            this.btnRemove.TabIndex = 13;
+            this.btnRemove.Text = "Disable";
+            this.btnRemove.UseVisualStyleBackColor = false;
+            this.btnRemove.Click += new System.EventHandler(this.BtnDisable_Click);
+            // 
+            // chcklstEnabled
+            // 
+            this.chcklstEnabled.CheckOnClick = true;
+            this.chcklstEnabled.FormattingEnabled = true;
+            this.chcklstEnabled.Location = new System.Drawing.Point(14, 147);
+            this.chcklstEnabled.Name = "chcklstEnabled";
+            this.chcklstEnabled.Size = new System.Drawing.Size(192, 246);
+            this.chcklstEnabled.TabIndex = 14;
+            // 
+            // btnEnable
+            // 
+            this.btnEnable.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(68)))), ((int)(((byte)(139)))));
+            this.btnEnable.FlatAppearance.BorderSize = 0;
+            this.btnEnable.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEnable.ForeColor = System.Drawing.Color.White;
+            this.btnEnable.Location = new System.Drawing.Point(119, 399);
+            this.btnEnable.Name = "btnEnable";
+            this.btnEnable.Size = new System.Drawing.Size(83, 33);
+            this.btnEnable.TabIndex = 15;
+            this.btnEnable.Text = "Enable";
+            this.btnEnable.UseVisualStyleBackColor = false;
+            this.btnEnable.Click += new System.EventHandler(this.BtnEnable_Click);
+            // 
+            // lstbxRSSI
+            // 
+            this.lstbxRSSI.FormattingEnabled = true;
+            this.lstbxRSSI.HorizontalScrollbar = true;
+            this.lstbxRSSI.ItemHeight = 21;
+            this.lstbxRSSI.Location = new System.Drawing.Point(16, 454);
+            this.lstbxRSSI.Name = "lstbxRSSI";
+            this.lstbxRSSI.Size = new System.Drawing.Size(190, 214);
+            this.lstbxRSSI.TabIndex = 10;
+            this.lstbxRSSI.Visible = false;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(1315, 691);
+            this.Controls.Add(this.btnEnable);
+            this.Controls.Add(this.chcklstEnabled);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.lstbxRSSI);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblSerialNumber);
+            this.Controls.Add(this.txtSerialNumber);
+            this.Controls.Add(this.pnlHeader);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Main";
+            this.Text = "Main";
+            this.pnlHeader.ResumeLayout(false);
+            this.pnlHeader.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.LOGO)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlHeader;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblHeader;
+        private System.Windows.Forms.TextBox txtSerialNumber;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnMinimize;
+        private System.Windows.Forms.Label lblSerialNumber;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.PictureBox LOGO;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.CheckedListBox chcklstEnabled;
+        private System.Windows.Forms.Button btnEnable;
+        private System.Windows.Forms.ListBox lstbxRSSI;
+    }
+}
+
---
+++ RSSI/Main.cs
@@ -0,0 +1,203 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.Office.Interop.Excel;
+
+namespace RSSI
+{
+    public partial class Main : Form
+    {
+        private bool isFormBeingDragged;
+
+        string netPath = @"\\exaculog02\ET1-Logs\", RSSI, SerialNumber, fileName= $@"Q:\Roberto\Excel\Test.xlsx";
+        HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+        DirectoryInfo Folder;
+        private int mouseDownX, mouseDownY;
+        DateTime time;
+        List<int> RSSIs = new List<int>();
+        int rssi, numSeries = 0;
+        Microsoft.Office.Interop.Excel.Application xlApp;
+        Workbook xlWorkBook;
+        Worksheet xlWorkSheet;
+        Range xlRange;
+
+        public Main()
+        {
+            InitializeComponent();
+            chart1.Series.Clear();
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+
+            Cursor.Current = Cursors.WaitCursor;
+            SerialNumber = txtSerialNumber.Text;
+
+            if (SerialNumber.Length != 7)
+            {
+                MessageBox.Show("Incorrect Serial Number", "ERROR");
+                goto _end;
+            }
+
+            try
+            {
+                Folder = new DirectoryInfo(netPath).GetDirectories(SerialNumber).First();
+            }
+            catch
+            {
+                MessageBox.Show("Folder Not Found", "Folders");
+                goto _end;
+            }
+            //if (Folder == null)
+            //{
+            //    MessageBox.Show("Folder Not Found", "Folders");
+            //    goto _end;
+            //}
+
+            openFile();
+
+            if (!chcklstEnabled.Items.Contains(SerialNumber))
+            {
+                chcklstEnabled.Items.Add(SerialNumber);
+
+                chart1.Series.Add(SerialNumber);
+                chart1.Series[numSeries].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+                chart1.Series[numSeries].BorderWidth = 2;
+                chart1.Series[numSeries].XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.DateTime;
+
+
+                var Files = new DirectoryInfo(Folder.FullName).GetFiles().Where(f => (f.Length > 0)).OrderBy(f => f.CreationTime);
+                int i = 1; 
+                foreach (var file in Files)
+                {
+                    int j = 1;
+                    time = file.LastWriteTime;
+                    xlWorkSheet.Cells[i, j++] = time.ToString("MM-dd-yyyy HH.mm.ss");
+
+                    doc.Load(file.FullName);
+                    var Nodes = doc.DocumentNode.SelectNodes("//text()[normalize-space(.) !='']").Where(n => n.InnerText.Contains("RSSI:"));
+                    if (Nodes.Count() > 0)
+                    {
+                        foreach (var node in Nodes)
+                        {
+                            rssi = Int32.Parse(node.InnerText.Substring(node.InnerText.IndexOf("RSSI:") + 5, 2));
+
+                            xlWorkSheet.Cells[i, j++] = rssi;
+                            RSSIs.Add(rssi);
+                        }
+
+                        RSSI = RSSIs.Average().ToString();
+                        RSSIs.Clear();
+
+                        chart1.Series[numSeries].Points.AddXY(time, Double.Parse(RSSI)); ;
+                        i++;
+                   
+                    }
+                }
+                numSeries++;
+            }
+
+            xlApp.ScreenUpdating = false;
+            xlApp.DisplayAlerts = false;
+
+            xlWorkBook.Close(true, fileName);
+
+            xlApp.ScreenUpdating = true;
+            xlApp.DisplayAlerts = true;
+_end:
+            Cursor.Current = Cursors.Default;
+            txtSerialNumber.Focus();
+            txtSerialNumber.SelectAll();
+        }
+
+        private void openFile()
+        {
+            // Opening or creating new file for unit
+            fileName = $@"Q:\Roberto\Excel\{SerialNumber}.xlsx";
+            xlApp = new ApplicationClass();
+            if (File.Exists(fileName))
+            {
+                xlWorkBook = xlApp.Workbooks.Open(fileName);
+            }
+            else
+            {
+                xlWorkBook = xlApp.Workbooks.Add();
+            }
+            xlWorkSheet = (Worksheet)xlWorkBook.Worksheets.Add();
+
+            xlWorkSheet.Name = DateTime.Now.ToString("MM-dd-yyyy HH.mm.ss");
+        }
+
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void BtnMinimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+         private void PnlHeader_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isFormBeingDragged = true;
+                mouseDownX = e.X;
+                mouseDownY = e.Y;
+            }
+        }
+
+        private void TxtSerialNumber_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter) BtnAdd_Click(sender, e);
+        }
+
+        private void PnlHeader_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isFormBeingDragged)
+            {
+                System.Drawing.Point temp = new System.Drawing.Point();
+
+                temp.X = this.Location.X + (e.X - mouseDownX);
+                temp.Y = this.Location.Y + (e.Y - mouseDownY);
+                this.Location = temp;
+                temp = new System.Drawing.Point();
+            }
+        }
+
+        private void PnlHeader_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isFormBeingDragged = false;
+            }
+        }
+
+        private void BtnEnable_Click(object sender, EventArgs e)
+        {
+            foreach (int i in chcklstEnabled.CheckedIndices)
+            {
+                chart1.Series[chcklstEnabled.Items[i].ToString()].Enabled = true;
+                chcklstEnabled.SetItemCheckState(i, CheckState.Unchecked);
+            }
+        }
+
+        private void BtnDisable_Click(object sender, EventArgs e)
+        {
+            foreach (int i in chcklstEnabled.CheckedIndices)
+            {
+                chart1.Series[chcklstEnabled.Items[i].ToString()].Enabled = false;
+                chcklstEnabled.SetItemCheckState(i, CheckState.Unchecked);
+            }
+        }
+    }
+}
